9f7cdc57b5f9131d6e6ca29287d255f43edbf8bb
empty-works <pama.mark@gmail.com>
2020-04-28 06:01:58 +0200

    Filled out main driver.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "Test.h"
 
 int main() {
 
+	std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr;
+	vec_ptr = make();
+	std::cout << "How many data points do you want to enter:";
+	int num;
+	std::cin >> num;
+	fill(*vec_ptr, num);
+	display(*vec_ptr);	
 	return 0;
 }
