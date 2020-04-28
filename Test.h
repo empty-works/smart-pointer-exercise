b0ee4332e128fb007d102bd938d107d147ae95ba
empty-works <pama.mark@gmail.com>
2020-04-28 05:48:14 +0200

    Added Test constructors.

--- Test.h
+++ Test.h
@@ -1,10 +1,16 @@
 #ifndef TEST_H_
 #define TEST_H_
 
+#include <iostream>
+
 class Test {
 
-	public:
+	private:
+	int data;
 
+	public:
+	Test() :data{0}{std::cout << "Test constructor(" << data << ")" << std::endl;}
+	Test(const int new_data) :data{new_data}{std::cout << "Test constructor(" << data << ")";}
 };
 
 #endif
